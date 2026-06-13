82928dfc6b40d5579214f2454a79dadf813cda70
zhangzhaosen <zhangzhaosen@sina.com>
2026-06-13 15:40:23 +0800

    feat: sign in &sign up

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,14 +1,23 @@
-
+'use client'
 import MuiNextLink from "@/components/MuiNextLink";
 import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from 'next/link'
+import createUser from "./create-user";
+import { useActionState } from "react";
+import { useFormState } from "react-dom";
 export default function Signup() {
+  const [state, formAction] = useFormState(createUser, { error: "" })
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" variant="outlined" type="email" />
-      <TextField label="Password" variant="outlined" type="password" />
-      <Button variant="contained" >Signup</Button>
-      <MuiNextLink href="/auth/login" className="self-center">Login</MuiNextLink>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField name="email" label="Email" variant="outlined" type="email"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField name="password" label="Password" variant="outlined" type="password" />
+        <Button type="submit" variant="contained" >Signup</Button>
+        <MuiNextLink href="/auth/login" className="self-center">Login</MuiNextLink>
+      </Stack>
+    </form>
   )
 }
