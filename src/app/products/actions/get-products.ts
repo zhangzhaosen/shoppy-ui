b147ad2747b4177a9ddcd888bf8ac53e01773c7f
zhangzhaosen <zhangzhaosen@sina.com>
2026-06-17 08:25:41 +0800

    feat: product filter

--- src/app/products/actions/get-products.ts
+++ src/app/products/actions/get-products.ts
@@ -4,5 +4,7 @@ import { get } from "@/app/common/util/fetch"
 import { Product } from "../interfaces/product.interface"
 
 export default async function getProducts(){
-  return await get<Product[]>("products", ["products"])
+  return await get<Product[]>("products", ["products"], new URLSearchParams({
+    status: 'availible'
+  }))
 }
