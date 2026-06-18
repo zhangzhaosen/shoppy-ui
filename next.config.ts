419302e2aa99463358be37102b5122155ce4cf50
zhangzhaosen <zhangzhaosen@sina.com>
2026-06-18 23:18:19 +0800

    feat: NEXT_PUBLIC_PRODUCT_IMAGE_HOST

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_PRODUCT_IMAGE_HOST||  "shoppy-backend.canoond.com",
+        hostname:  "shoppy-backend.canoond.com",
         port: "", // 标准443 HTTPS，无自定义端口，必须写空字符串精准匹配
         pathname: '/images/products/**',
       },
