b11315dd921faa3802829375718bc01a171a82f0
zhangzhaosen <zhangzhaosen@sina.com>
2026-06-18 22:54:47 +0800

    feat: PRODUCT_IMAGE_HOST & protocol&port

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: process.env.PRODUCT_IMAGE_HOST!,
-      
+        port: "", // 标准443 HTTPS，无自定义端口，必须写空字符串精准匹配
         pathname: '/images/products/**',
       },
     ],
