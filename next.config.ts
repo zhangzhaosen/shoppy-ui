298971ec34e807f8394028930a2c7d3703bd75d8
zhangzhaosen <zhangzhaosen@sina.com>
2026-06-19 09:44:59 +0800

    feat: s3 image upload

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.PRODUCT_IMAGE_HOST||  "shoppy-backend.canoond.com",
+        hostname: process.env.PRODUCT_IMAGE_HOST||  "canoon-shoppy-products.s3.us-east-1.amazonaws.com",
         port: "", // 标准443 HTTPS，无自定义端口，必须写空字符串精准匹配
-        pathname: '/images/products/**',
+        //pathname: '/images/products/**',
       },
     ],
   },
--- src/app/products/product-image.ts
+++ src/app/products/product-image.ts
@@ -1,5 +1,6 @@
 import { API_URL } from "../common/constants/api";
 
 export function getProductImage(productId:number){
-  return `${API_URL}/images/products/${productId}.png`
+ // return `${API_URL}/images/products/${productId}.png`
+ return `${process.env.NEXT_PUBLIC_PRODUCT_IMAGE_URL}/${productId}.png`
 }
